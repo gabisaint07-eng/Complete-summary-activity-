e83de7042d820a064d219ff947663bde9330894a
GabiSaint <gasaint07@gmail.com>
2025-09-05 12:07:15 -0400

    print distance

--- SoftDev02/line_distance.py
+++ SoftDev02/line_distance.py
@@ -1,1 +1,8 @@
+def distance (x1, y1, x2, y2):
+    return(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
 
+distance(55, 12, 180, 74)
+distance(3, 5, 3, 2) 
+distance(1234, 1, 23, 6)
+total = distance(55, 12, 180, 74) + distance(3, 5, 3, 2) + distance(1234, 1, 23, 6)
+print("Total Distance of all three line segments = ",total)        
