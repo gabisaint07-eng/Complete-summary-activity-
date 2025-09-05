bcac0fdb7a1b5213120ee5e7c729e72ff2acae82
GabiSaint <gasaint07@gmail.com>
2025-09-05 11:50:31 -0400

    print total

--- SoftDev02/distances.py
+++ SoftDev02/distances.py
@@ -1,7 +1,7 @@
 def distance (x1, y1, x2, y2):
-    print(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
+   return(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
 
-distance(55, 12, 180, 74)
-distance(3, 5, 3, 2) 
-distance(1234, 1, 23, 6)
+print(55, 12, 180, 74)
+print(3, 5, 3, 2) 
+print(1234, 1, 23, 6)
 
