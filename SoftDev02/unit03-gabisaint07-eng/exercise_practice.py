204f13b783ede07958bab36ae36326102a5e78d7
GabiSaint <gasaint07@gmail.com>
2025-09-24 02:47:58 -0400

    Returmnnn

---
+++ SoftDev02/unit03-gabisaint07-eng/exercise_practice.py
@@ -0,0 +1,31 @@
+def square(num):
+    numSquared = int 
+    numSquared = (num * num)
+    return numSquared
+    
+
+
+def main():
+    print(square(4))
+main()
+
+def add(num1,num2):
+    return num1 + num2
+print (add (3,4))
+
+def greet(name): 
+    return "Hello, " + name 
+print (greet ("Gabi"))
+
+def is_even(x):
+    return x % 2 == 0 
+print (is_even(4))
+print (is_even(5))
+
+def check_number(num):
+    return 
+        if 
+    pass
+print(check_number(4))
+print(check_number(7))
+
